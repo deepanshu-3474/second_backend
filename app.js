8268859a27496fad262f4e77364ede1a35538a6a
deepanshu <anshukeshri64@gmail.com>
2025-04-17 11:36:09 +0530

    fifth-commit

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import express, { application } from 'express';
 import accountRouter from './src/routes/AccountRouter.js';
 import employeeRouter from './src/routes/EmployeeRouter.js';
-
+import {path} from 'path'
 
 const app = express();
 app.use(express.json()); // express middleware
 app.use(express.urlencoded({extended:true})) // express middleware
 
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get("/",function(req,res){
-    res.send("Wow !!")
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 app.use("/api/v1/account",accountRouter);
 app.use("/api/v1/employee",employeeRouter);
 // app.get("/api/v1/info/:name",search);
