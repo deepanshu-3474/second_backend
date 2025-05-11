de6e1c15a2ddab1aa1edd9df9adbf9ddcad196df
deepanshu <anshukeshri64@gmail.com>
2025-05-11 12:16:27 +0530

    deleteEmployee

--- src/controller/EmployeeController.js
+++ src/controller/EmployeeController.js
@@ -1,160 +1,141 @@
+// file: controller/EmployeeController.js
 import axios from "axios";
 import EmployeeModel from "../model/EmployeeModel.js";
-export async function addEmployee(req,res,next){
-    try{
-        const data = req.body;
-        const empData = {
-            emp_name:data.emp_name,
-            emp_email:data.emp_email,
-            emp_job_city:data.emp_job_city,
-            emp_maritail_status:data.emp_maritail_status,
-            emp_age:data.emp_age
-        }
+import { validationResult } from "express-validator";
 
-   const empRes = await EmployeeModel.create(empData);
-       if(empRes)
-       {
-        res.json(
-            {
-                status:"success",
-                data:empData
-            }
-        )
-       }
-        // res.json({
-        //    jjjjjjjjjjjjjjjjjjjjjjj:data.emp_name
-        // })
-       
-    }
-    catch(error)
-    {
-        const resError = {}
-          resError.status = "failed"
-          if (error.name === "ValidationError") {
-              let errors = {};
-              Object.keys(error.errors).forEach((key) => {
-                errors[key] = error.errors[key].message;
-              });
-              resError.error = errors;
-            }
-          res.json(resError)
-    }
-
-  
-
-}
- export async function updateEmployee(req,res,next) {
-     try{
-        
-        // const data = req.params; // for get data from url
-
-        // const data = req.query;
-        // console.log(data);
-
-        const {id} = req.params;
-        
-        //const data = req.query;
-
-        
-        
-        const data = req.body;
-        const updateEmployeeData = {
-        emp_name:data.emp_name,
-        emp_email:data.emp_email,
-        emp_job_city:data.emp_job_city,
-        emp_maritail_status:data.emp_maritail_status,
-        emp_age:data.emp_age
-
-    }
-
-    const empUpdateres = await EmployeeModel.updateOne({_id:id},updateEmployeeData)
-    if(empUpdateres){
-        res.json(
-            {
-                status:"sucess",
-                message:"data updated",
-                data:empUpdateres
-                
-            }
-        )
-    }
-    // res.json({
-    //     id:id,
-    //     data: data
-    // })
-}
-    catch(error){
-        const resError = {}
-          resError.status = "failed"
-          console.error(error)
-          if (error.name === "ValidationError") {
-              let errors = {};
-              Object.keys(error.errors).forEach((key) => {
-                errors[key] = error.errors[key].message;
-              });
-              resError.error = errors;
-            }
-          res.json(resError)
-
-    }
-    }
-
- export async function deleteEmployee(req,res,next) {
-
-    const data = req.body;
-    const emp_id = data._id;
-
-    const deleteEmp = await EmployeeModel.deleteOne({_id:emp_id})
-    if(deleteEmp){
-        res.json({
-            status: "successfully deleted",
-            data: data
-        })
-    }
-    
- }
- export async function getEmployee(req,res,next) {
-    try{
-        const empData = await EmployeeModel.find();
-        res.json({
-            status: "success",
-            data: empData,
+// Utility to handle validation errors
+function handleValidationError(res, error) {
+    if (error.name === "ValidationError") {
+        let errors = {};
+        Object.keys(error.errors).forEach((key) => {
+            errors[key] = error.errors[key].message;
+        });
+        return res.status(400).json({
+            status: "failed",
+            error: errors
         });
     }
-    catch(error){
-        res.json({
-            message: "data not found"
-        })
-    }
-    
-    
- }
- export async function singleEmployee(req,res,next) {
-    // const data = req.body;
-
-    try{
-    const {id} = req.params;
-
-    const singleEmp =  await EmployeeModel.findById({_id:id})
-    if(singleEmp){
-        res.json({
-            status: "Employee Found",
-            data: singleEmp
-        })
-    }
-}catch(error){
-    console.error(error)
-    res.json({
-        status:"not founs",
-        message:error
-    })
-
+    res.status(500).json({
+        status: "error",
+        message: error.message
+    });
 }
 
- }
- export async function searchEmployee(req, res, next) {
+// Add New Employee
+export async function addEmployee(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ status: "failed", errors: errors.array() });
+    }
+
     try {
-        console.log(req.query)
+        const empData = req.body;
+        const empRes = await EmployeeModel.create(empData);
+        res.status(201).json({
+            status: "success",
+            data: empRes
+        });
+    } catch (error) {
+        handleValidationError(res, error);
+    }
+}
+
+// Update Employee by ID
+export async function updateEmployee(req, res, next) {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+
+        const empUpdateres = await EmployeeModel.updateOne({ _id: id }, data);
+
+        if (empUpdateres.matchedCount === 0) {
+            return res.status(404).json({
+                status: "failed",
+                message: "Employee not found"
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Employee updated",
+            data: empUpdateres
+        });
+    } catch (error) {
+        handleValidationError(res, error);
+    }
+}
+
+// Delete Employee by ID
+export async function deleteEmployee(req, res, next) {
+    try {
+        const emp_id = req.params.id;
+
+        const deleteEmp = await EmployeeModel.deleteOne({ _id: emp_id });
+
+        if (deleteEmp.deletedCount === 0) {
+            return res.status(404).json({
+                status: "failed",
+                message: "Employee not found"
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Employee deleted",
+            data: deleteEmp
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: error.message
+        });
+    }
+}
+
+// Get All Employees
+export async function getEmployee(req, res, next) {
+    try {
+        const empData = await EmployeeModel.find();
+        res.status(200).json({
+            status: "success",
+            data: empData
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Data not found"
+        });
+    }
+}
+
+// Get Single Employee by ID
+export async function singleEmployee(req, res, next) {
+    try {
+        const { id } = req.params;
+        const singleEmp = await EmployeeModel.findById(id);
+
+        if (!singleEmp) {
+            return res.status(404).json({
+                status: "failed",
+                message: "Employee not found"
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            data: singleEmp
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: error.message
+        });
+    }
+}
+
+// Search Employees by Name (partial match)
+export async function searchEmployee(req, res, next) {
+    try {
         const { name } = req.query;
 
         const employees = await EmployeeModel.find({
@@ -166,9 +147,7 @@ export async function addEmployee(req,res,next){
             count: employees.length,
             data: employees
         });
-
     } catch (error) {
-        console.error(error);
         res.status(500).json({
             status: "error",
             message: "Internal Server Error"
@@ -176,18 +155,17 @@ export async function addEmployee(req,res,next){
     }
 }
 
-
-
- export async function getDetails(req, res, next) {
+// Get External Employee Details from API
+export async function getDetails(req, res, next) {
     try {
         const data = await axios.get('https://reqres.in/api/users?page=2');
-        res.json({
+        res.status(200).json({
+            status: "success",
             data: data.data
         });
     } catch (error) {
-        console.error(error);
-        res.json({
-            status: "not found",
+        res.status(500).json({
+            status: "error",
             message: error.message
         });
     }
--- src/model/EmployeeModel.js
+++ src/model/EmployeeModel.js
@@ -1,45 +1,48 @@
-import "../db/db.js"
+// file: model/EmployeeModel.js
 import mongoose from "mongoose";
-import COLLECTION from "../db/collection.js";
+import COLLECTION from "../db/collection.js"; // Ensure this exports a string like { employeedetails: 'employees' }
 
-
-// schema is validitor
-// orm object relational model : 
+// Employee Schema Definition
 const EmployeeSchema = new mongoose.Schema({
-    emp_name:{type:String,required:[true,"Employee name is required"],validate:{
-        validator:function(v){
-            return /^[a-zA-Z ]{2,30}$/; // schema validator
-        },
-        message:props=>`{$props.value} is not valid name`
-    }},
-
-
-    emp_email:{
-        type:String,
-        unique:true,
+    emp_name: {
+        type: String,
+        required: [true, "Employee name is required"],
         validate: {
-        validator: function(v) {
-          return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
-        },
-        message: props => `${props.value} is not a valid email!`
-      },
-      required: [true, 'email is required']
-    },
-
-    emp_job_city:[
-            {city:{type:String}}
-    ],
-    emp_maritail_status:{
-        type:String,
-        enum:{
-            values:["Single", "Married"],
-            message: ` not supported`
+            validator: function (v) {
+                return /^[a-zA-Z ]{2,30}$/.test(v); // Properly test the name pattern
+            },
+            message: props => `${props.value} is not a valid name`
         }
     },
-    // emp_age:{type:Number,min:18,max:100},
 
-},{timestamps:true})
+    emp_email: {
+        type: String,
+        unique: true,
+        required: [true, 'Email is required'],
+        validate: {
+            validator: function (v) {
+                return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
 
-const EmployeeModel = mongoose.model(COLLECTION.employeedetails,EmployeeSchema)
+    emp_job_city: [
+        {
+            city: { type: String }
+        }
+    ],
+
+    emp_maritail_status: {
+        type: String,
+        enum: {
+            values: ["Single", "Married"],
+            message: 'Marital status must be either "Single" or "Married"'
+        }
+    }
+
+}, { timestamps: true });
+
+const EmployeeModel = mongoose.model(COLLECTION.employeedetails, EmployeeSchema);
 
 export default EmployeeModel;
